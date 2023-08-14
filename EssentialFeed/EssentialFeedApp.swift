f1e6b9996d7c0c46bcaca5cd0916476eceb19d62
Wiljay Flores <universe@wiljays-air.lan>
2023-08-14 14:27:24 -0400

    Created FeedItem Model and FeedLoader interface

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Wiljay Flores on 2023-08-14.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
