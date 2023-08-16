97f810278768785d24403b39a7a6cd94254a2dcd
Wiljay Flores <wiljay.dev@gmail.com>
2023-08-15 21:34:04 -0400

    Added memory leak detection

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -100,11 +100,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     // MARK: - Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
-        
+        detectMemoryLeak(sut, file: file, line: line)
+        detectMemoryLeak(client, file: file, line: line)
         return (sut, client)
+    }
+    
+    private func detectMemoryLeak(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "This instance should have been deallocated. Potential Memory Leak", file: file, line: line)
+        }
     }
     
     private func makeItem(
